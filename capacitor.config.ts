17775928a2b82856dad8eb48d3a459c0b43c3545
knapkova <118272531+knapkova@users.noreply.github.com>
2024-11-23 13:34:17 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'TradeTracker',
+  webDir: 'www'
+};
+
+export default config;
