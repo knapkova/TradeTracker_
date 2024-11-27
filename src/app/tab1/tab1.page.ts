0871f81892ede674d7aef5a3d9dceb110f5bab83
knapkova <118272531+knapkova@users.noreply.github.com>
2024-11-27 17:50:55 +0100

    add proxy settings

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Trading212Service } from 'src/app/api/trading212.service';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
+  tradingInfo: any;
 
-  constructor() {}
+  constructor(private tradingService: Trading212Service) {}
 
+  ngOnInit(): void {
+    this.tradingService.getTradingInfo().subscribe((data) => {
+      console.log(data);
+      this.tradingInfo = data;
+    });
+  }
 }
